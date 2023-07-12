3f297b46fc35e470eb4e30abd730727b9fbd151a
charlotteg1 <charlotte.gould3@gmail.com>
2023-07-12 21:17:24 +0100

    set up of bank account

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,4 +1,66 @@
 public class BankAccount {
-    Private String firstName;
-    Private String lastName;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String accountNumber;
+    private double balance = 0;
+
+    public BankAccount(String firstName, String lastName, String dateOfBirth,String accountNumber, double balance){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth =dateOfBirth;
+        this.accountNumber= accountNumber;
+        this.balance = balance;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public void setFirstName(String firstName){
+
+        this.firstName= firstName;
+    }
+    public String getLastName(){
+
+        return this.lastName;
+    }
+    public void setLastName(String lastName){
+
+        this.lastName= lastName;
+    }
+    public String getDateOfBirth(){
+
+        return this.dateOfBirth;
+    }
+    public void setDateOfBirth(String dateOfBirth){
+
+        this.dateOfBirth= dateOfBirth;
+    }
+    public String getAccountNumber(){
+
+        return this.accountNumber;
+    }
+    public void setAccountNumber(String accountNumber){
+
+        this.accountNumber= accountNumber;
+    }
+    public double getBalance(){
+
+        return this.balance;
+    }
+    public void setBalance(double balance){
+
+        this.balance= balance;
+    }
+    public void deposit(double amount){
+        this.balance=this.balance + amount;
+    }
+    public void withdrawal(double amount){
+        this.balance =this.balance - amount;
+    }
+    public void interest(){
+        this.balance = this.balance*1.03;
+    }
+
+
+
 }
---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,160 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BankAccountTest {
+
+
+    @Test
+    public void canReturnFirstName(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        String result= bankAccount.getFirstName();
+
+        String expected = "John";
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canReturnLastName(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        String result= bankAccount.getLastName();
+
+        String expected = "Smith";
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canReturnDateOfBirth() {
+
+        BankAccount bankAccount = new BankAccount("John", "Smith", "11/08/1991", "12345678", 3000);
+
+        String result = bankAccount.getDateOfBirth();
+
+        String expected = "11/08/1991";
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canReturnAccountNumber(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        String result= bankAccount.getAccountNumber();
+
+        String expected = "12345678";
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canReturnBalance(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        double result= bankAccount.getBalance();
+
+        double expected = 3000;
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canSetFirstName(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        bankAccount.setFirstName("John");
+        String result= bankAccount.getFirstName();
+
+        String expected = "John";
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canSetLastName(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        bankAccount.setFirstName("Smith");
+        String result= bankAccount.getLastName();
+
+        String expected = "Smith";
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canSetDateOfBirth(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        bankAccount.setDateOfBirth("11/08/1991");
+        String result= bankAccount.getDateOfBirth();
+
+        String expected = "11/08/1991";
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canSetAccountNumber(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        bankAccount.setDateOfBirth("12345678");
+        String result= bankAccount.getAccountNumber();
+
+        String expected = "12345678";
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canSetBalance(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        bankAccount.setBalance(3000);
+        double result= bankAccount.getBalance();
+
+        double expected = 3000;
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canDeposit(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        bankAccount.deposit(500);
+        double result= bankAccount.getBalance();
+
+        double expected = 3500;
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canWithdraw(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        bankAccount.withdrawal(800);
+        double result= bankAccount.getBalance();
+
+        double expected = 2200;
+
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    public void canAddInterest(){
+
+        BankAccount bankAccount = new BankAccount("John","Smith", "11/08/1991","12345678",3000);
+
+        bankAccount.interest();
+        double result= bankAccount.getBalance();
+
+        double expected = 3090;
+
+        assertThat(result).isEqualTo(expected);
+    }
+}
